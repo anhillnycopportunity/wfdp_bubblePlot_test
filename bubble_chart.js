e98ac3b9637baafdeb437efbc2ef92bd3204f3b5
anhillnycopportunity <anhill@nycopportunity.nyc.gov>
2026-06-15 16:13:34 -0400

    remove grid lines

--- bubble_chart.js
+++ bubble_chart.js
@@ -182,16 +182,16 @@ function initChart(csvText, containerId = "chart-container") {
         formatter() {
           return Highcharts.numberFormat(this.value, 0, ".", ",");
         },
-      },
-      plotLines: [
-        {
-          value: 0,
-          color: "#999",
-          width: 1,
-          dashStyle: "Solid",
-          zIndex: 3,
-        },
-      ],
+      } // ,
+     // plotLines: [
+     //   {
+     //     value: 0,
+     //     color: "#999",
+     //     width: 1,
+     //     dashStyle: "Solid",
+     //     zIndex: 3,
+     //   },
+     // ],
     },
 
      yAxis: {
